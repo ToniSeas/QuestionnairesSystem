2989898f5b302562beb30771a1447bf19a227079
Susan Sojo Castro <sojos204@gmail.com>
2022-09-25 17:30:12 -0600

    Desarrollo interfaz de crear y resutados cuestionarios

---
+++ frontend/src/app/components/create-questionnaire/create-step-one/create-step-one.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CreateStepOneComponent } from './create-step-one.component';
+
+describe('CreateStepOneComponent', () => {
+  let component: CreateStepOneComponent;
+  let fixture: ComponentFixture<CreateStepOneComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CreateStepOneComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(CreateStepOneComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
