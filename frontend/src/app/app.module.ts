2ebed6d62bd33c602eabd48a370fd665ae9a3d73
Heiner <heinermongelopez@gmail.com>
2022-10-17 16:09:27 -0600

    Se agrega import necesario

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -13,21 +13,21 @@ import { CreateStepOneComponent } from './components/create-questionnaire/create
 import { SearchQuestionnaireComponent } from './components/search-questionnaire/search-questionnaire.component';
 import { QuestionnaireResultsComponent } from './components/questionnaire-results/questionnaire-results.component';
 import { ManageCategoryComponent } from './components/manage-category/manage-category.component';
-import {MatStepperModule} from '@angular/material/stepper';
-import {MatPaginatorModule} from '@angular/material/paginator';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatPaginatorModule } from '@angular/material/paginator';
 import { NotFoundPageComponent } from './components/not-found-page/not-found-page.component';
 import { SelectOfficeComponent } from './components/login/select-office/select-office.component';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
-import {MatMenuModule} from '@angular/material/menu';
-import {MatSelectModule} from '@angular/material/select';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSelectModule } from '@angular/material/select';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ManageSubcategoryComponent } from './components/manage-subcategory/manage-subcategory.component';
 import { CreateQuestionDialog, CreateStepTwoComponent } from './components/create-questionnaire/create-step-two/create-step-two.component';
 import { CreateStepThreeComponent } from './components/create-questionnaire/create-step-three/create-step-three.component';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatTableModule } from '@angular/material/table';
@@ -64,11 +64,12 @@ import { MatTableModule } from '@angular/material/table';
     MatSelectModule,
     MatButtonModule,
     BrowserAnimationsModule,
-    ReactiveFormsModule,
     MatInputModule,
     MatDialogModule,
     MatFormFieldModule,
-    MatTableModule
+    MatTableModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
