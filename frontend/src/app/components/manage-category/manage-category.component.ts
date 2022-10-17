6f5689c60dabf371d0a3438460804f363771352c
sojos204 <67076560+sojos204@users.noreply.github.com>
2022-10-16 20:51:33 -0600

    Cambios Formularios

--- frontend/src/app/components/manage-category/manage-category.component.ts
+++ frontend/src/app/components/manage-category/manage-category.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { Category } from 'src/app/models/Category';
@@ -16,17 +17,28 @@ export class ManageCategoryComponent implements OnInit {
 
   private displayedColumns: string[] = ['title', 'operations'];
   private dataSource = new MatTableDataSource<Category>;
+  checkoutForm!: FormGroup;
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild('nameCategoryQuestion') nameCategoryQuestion: any; // accessing the reference element
 
-  constructor(private categoryService: CategoryService) { }
+  private createCategoryForm!: FormGroup;
+
+  constructor(private categoryService: CategoryService, private formBuilder: FormBuilder) { 
+    this.checkoutForm = this.formBuilder.group({
+      name: ''});
+  }
+  
+
+
+  public getFormGroup(): FormGroup { return this.createCategoryForm; }
 
   ngOnInit(): void {
     this.categoryService.getCategories().subscribe(
       (result: Category[]) => (this.updateCategoryList(result))
     )
     this.dataSource.paginator = this.paginator;
+
   }
 
   public getDataSource(): MatTableDataSource<Category> {
@@ -54,6 +66,13 @@ export class ManageCategoryComponent implements OnInit {
 
   public clearInput(): void {
     this.nameCategoryQuestion.nativeElement.value = ' ';
+  }
+
+  onSubmit(customerData:string) {
+    // Process checkout data here
+    this.checkoutForm.reset();
+    //this.createCategory(customerData);
+    console.warn('Your order has been submitted', customerData);
   }
 
 }
