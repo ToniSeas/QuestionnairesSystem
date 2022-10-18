8000fc216060be2fbeb6b4471736f3ca905e167f
M0nsai <saygonza23@gmail.com>
2022-10-17 22:23:02 -0600

    cuestionario paso 1. Todos los datos van del frontend y llegan al metodo createQuestionnaire de create-step-one.ts donde se crea el cuestionario

--- frontend/src/app/components/create-questionnaire/create-step-one/create-step-one.component.ts
+++ frontend/src/app/components/create-questionnaire/create-step-one/create-step-one.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Questionnaire } from 'src/app/models/Questionnaire';
+import { CategoryService } from 'src/app/services/category.service';
 
 @Component({
   selector: 'app-create-step-one',
@@ -7,9 +10,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateStepOneComponent implements OnInit {
 
+  private createStepOneComponentForm!: FormGroup;
+
+
+  public questionnaire!: Questionnaire;
+
   constructor() { }
+  public getFormGroup(): FormGroup { return this.createStepOneComponentForm; }
+
 
   ngOnInit(): void {
+
+    this.createStepOneComponentForm = new FormGroup({
+      questionnaireName: new FormControl("", [Validators.required]),
+      questionnaireState: new FormControl("",[Validators.required]),
+      questionnaireDescription: new FormControl("",[Validators.required]),
+      questionnaireDate: new FormControl("",[Validators.required]),
+      questionnaireType: new FormControl("",[Validators.required])
+    })
+
+  }
+
+  onSubmit = () => {
+    if (this.createStepOneComponentForm.invalid) {
+      console.log('test')
+    } else {
+      console.log('válido');
+      let nameQ= this.createStepOneComponentForm.get('questionnaireName')?.value;
+      let typeQ= this.createStepOneComponentForm.get('questionnaireType')?.value;
+      let dateQ= this.createStepOneComponentForm.get('questionnaireDate')?.value;
+      let descriptionQ= this.createStepOneComponentForm.get('questionnaireDescription')?.value;
+      let stateQ= this.createStepOneComponentForm.get('questionnaireState')?.value;
+      this.createQuestionnaire(nameQ, typeQ, dateQ, descriptionQ, stateQ);
+      this.createStepOneComponentForm.reset();
+    }
+  }
+
+  public createQuestionnaire(nameQ: string, typeQ: string, dateQ: Date, descriptionQ: string, stateQ: string): void {
+    this.questionnaire = new Questionnaire({type: typeQ, name: nameQ, date: dateQ, description: descriptionQ, state: stateQ});
   }
 
 }
--- frontend/src/app/models/Questionnaire.ts
+++ frontend/src/app/models/Questionnaire.ts
@@ -2,12 +2,19 @@ import { Question } from "./Question";
 
 export class Questionnaire{
   
-    questions?: Question[];
-    type?:String;
-    name?:String;
-    date?: Date;
-    description?: String;
-  
-    constructor() {}
-    
+  //questions?: Question[];
+  type?:String;
+  name?:String;
+  date?: Date;
+  description?: String;
+  state?: String;
+
+  constructor({type, name, date, description, state}:{type?:String, name?:String, date?:Date, description?: String, state?: String}) {
+    this.type=type;
+    this.name=name;
+    this.date=date;
+    this.description=description;
+    this.state=state;
   }
+    
+}
