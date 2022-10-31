9ec63f63e026dcae96029d3f1b07493a518d122e
heiner <heinermongelopez@gmail.com>
2022-10-31 14:11:23 -0600

    Define el estilo inicial para la vista de responder cuestionarios

--- frontend/src/app/components/questionnaire-view/questionnaire-view.component.ts
+++ frontend/src/app/components/questionnaire-view/questionnaire-view.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Question } from 'src/app/models/Question';
+import { Questionnaire } from 'src/app/models/Questionnaire';
 
 @Component({
   selector: 'app-questionnaire-view',
@@ -7,7 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuestionnaireViewComponent implements OnInit {
 
-  constructor() { }
+
+  /*
+      public static readonly UNIQUE_SELECTION:string = "Selección Única";
+    public static readonly MULTIPLE_SELECTION:string = "Selección Múltiple";
+    public static readonly SCALE:string = "Escala";
+    public static readonly NUMERIC:string = "Numérica";
+    public static readonly TRUE_FALSE:string = "Verdadero o Falso";
+    public static readonly OPENED:string = "Abierta";
+   */
+
+  questionnaire?: Questionnaire
+
+  constructor() {
+    this.questionnaire = new Questionnaire({})
+    this.questionnaire.id = 0;
+    this.questionnaire.description = "Este es el cuestionario de pruebas utilizado durante el desarrollo del sistema de cuestionarios."
+    this.questionnaire.name = "Cuestionario de pruebas"
+  }
 
   ngOnInit(): void {
   }
