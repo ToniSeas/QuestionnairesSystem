c1d2e00379ffb553660d27ba923cfa28912db484
TONI_FOOS\tonis <toniseas01@gmail.com>
2022-11-03 22:54:51 -0600

    Bug Fixes

--- frontend/src/app/models/SubCategory.ts
+++ frontend/src/app/models/SubCategory.ts
@@ -3,11 +3,13 @@ export class SubCategory{
     name?: String
     id?: number;
     idCategory?: number;
+    editable?: boolean;
     
     constructor({name, id, idCategory}:{name?: String, id?: number, idCategory?: number}) {
         this.name=name;
         this.id=id;
         this.idCategory=idCategory;
+        this.editable=false;
     }
 
 }
