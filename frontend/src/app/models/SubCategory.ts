c8d1aa0e77007580f80cad857734f610102162af
TONI_FOOS\tonis <toniseas01@gmail.com>
2022-11-03 22:53:53 -0600

    Categorias y SubCategorias, Respuestas, Opciones, URL basado en roles

---
+++ frontend/src/app/models/SubCategory.ts
@@ -0,0 +1,13 @@
+export class SubCategory{
+
+    name?: String
+    id?: number;
+    idCategory?: number;
+    
+    constructor({name, id, idCategory}:{name?: String, id?: number, idCategory?: number}) {
+        this.name=name;
+        this.id=id;
+        this.idCategory=idCategory;
+    }
+
+}
