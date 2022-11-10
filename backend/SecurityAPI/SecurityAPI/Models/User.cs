14634b5b617d4acefd96cbc6bf75cbc62c477070
heiner <heinermongelopez@gmail.com>
2022-11-10 17:34:26 -0600

    El módulo de seguridad maneja las oficinas

--- backend/SecurityAPI/SecurityAPI/Models/User.cs
+++ backend/SecurityAPI/SecurityAPI/Models/User.cs
@@ -3,17 +3,26 @@
     public class User
     {
 
-        public User(String userName, String password, string role)
+        public User(String name, String userName, String password, String role, int idOffice, int id)
         {
             this.UserName = userName;
             this.Password = password;
             this.Role = role;
+            this.IdOffice = idOffice;
+            this.Name = name;
+            Id = id;
         }
         public String UserName { get; set; }
 
         public String Password { get; set; }
 
         public String Role { get; set; }
+
+        public int IdOffice { get; set; }
+
+        public String Name { get; set; }
+
+        public int Id { get; set; }
     }
 
 }
